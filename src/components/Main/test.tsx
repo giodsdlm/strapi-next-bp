58f6dc30a6ed09f66c302789f1e70ea22bc65674
Giovanna Dias <gio.dsdlm@gmail.com>
2023-09-14 22:35:28 -0300

    adding global styling

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,14 +5,24 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     //renderiza o componente
     const { container } = render(<Main />)
+
     //busca o elemento
     const mainComponent = screen.getByRole('heading', {
       name: /react avançado/i
     })
+
     //verifica a existencia dele
     expect(mainComponent).toBeInTheDocument()
 
     //gerar snapshpt
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    //renderiza o componente
+    const { container } = render(<Main />)
+
+    //verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
